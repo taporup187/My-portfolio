eaed84c89d65c67d1a9312c99ccd109cb8bb4703
Taporup Boliar <taporupboliar187@gmail.com>
2023-02-16 17:18:36 +0530

    minor changes

--- js/index.js
+++ js/index.js
@@ -1,14 +1,45 @@
+// pre loder
+window.addEventListener("load", () => {
+    document.getElementById("preloader").style.display = "none";
+})
+
+// nav shadow on scroll
 const nav = document.querySelector("nav");
 
-window.onscroll=()=>{
+window.onscroll = () => {
     scrollSwadow();
 }
-function scrollSwadow(){
-    if(document.body.scrollTop > 500 ||
-        document.documentElement.scrollTop > 500){
-         nav.style.boxShadow="0px 5px 8px #ddd";
+function scrollSwadow() {
+    if (document.body.scrollTop > 500 ||
+        document.documentElement.scrollTop > 500) {
+        nav.style.boxShadow = "0px 5px 8px #ddd";
     }
-    else{
-        nav.style.boxShadow="none";
+    else {
+        nav.style.boxShadow = "none";
     }
 }
+
+//gmail onclick copy to clipbord
+
+document.getElementById("gmail-coppy").addEventListener('click', function () {
+    copyToClipboard('taporupboliar187@gmail.com');
+});
+function copyToClipboard(text) {
+    var selected = false;
+    var el = document.createElement('textarea');
+    el.value = text;
+    el.setAttribute('readonly', '');
+    el.style.position = 'absolute';
+    el.style.left = '-9999px';
+    document.body.appendChild(el);
+    if (document.getSelection().rangeCount > 0) {
+        selected = document.getSelection().getRangeAt(0)
+    }
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+    if (selected) {
+        document.getSelection().removeAllRanges();
+        document.getSelection().addRange(selected);
+    }
+};
